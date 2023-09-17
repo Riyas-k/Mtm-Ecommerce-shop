584ccdb11a5b5c661bf41c171e42be0a5534d24b
Athif Binu <binuathif@gmail.com>
2023-09-17 17:53:37 +0530

    fix minor styles

--- client/src/components/Footer/Footer.jsx
+++ client/src/components/Footer/Footer.jsx
@@ -1,84 +1,62 @@
 // eslint-disable-next-line no-unused-vars
 import React from 'react';
 import { Link } from 'react-router-dom';
-import logo from "../../assets/Images/logo.jpeg";
-import "./Fooer.css"
-import "../../styles/Common.css"
-import "../../styles/responsive.css"
 const Footer = () => {
   return (
-    <footer className='footer'>
-    <div className="container grid grid--footer">
-     <div className="logo-col">
-       <Link to="/home" class="footer-logo">
-         <img className="logo" alt="Omnifood logo" src={logo} />
-       </Link>
+    <footer className='py-32'>
+    <div className="grid grid-cols-2 md:grid-cols-5 gap-8 container mx-auto">
+      <div className="col-span-2 md:col-span-1">
+        <Link to="/home" className="block mb-8">
+          <h1 className='font-semibold text-2xl text-orange-500'>M<span className='text-orange-500'>tm</span> Metales</h1>
+        </Link>
+        <ul className="flex gap-8">
+          <li><Link to="#" className="text-2xl text-gray-600 hover:text-gray-500"><ion-icon name="logo-instagram"></ion-icon></Link></li>
+          <li><Link to="#" className="text-2xl text-gray-600 hover:text-gray-500"><ion-icon name="logo-facebook"></ion-icon></Link></li>
+          <li><Link to="#" className="text-2xl text-gray-600 hover:text-gray-500"><ion-icon name="logo-twitter"></ion-icon></Link></li>
+        </ul>
+        <p className="text-gray-600 mt-auto">&copy; <span className="text-orange-500">2027</span> by Mtm, Inc. All rights reserved.</p>
+      </div>
 
-       <ul className="social-links">
-         <li>
-           <a className="footer-link" href="#"
-             ><ion-icon class="social-icon" name="logo-instagram"></ion-icon></a>
-         </li>
-         <li>
-           <a className="footer-link" href="#"
-             ><ion-icon class="social-icon" name="logo-facebook"></ion-icon></a>
-         </li>
-         <li>
-           <a className="footer-link" href="#"
-             ><ion-icon class="social-icon" name="logo-twitter"></ion-icon></a>
-         </li>
-       </ul>
+      <div className="md:col-span-1">
+        <p className="text-xl font-semibold mb-8">Contact us</p>
+        <address className="text-gray-600">
+          <p className="mb-4">623 Harrison St., 2nd Floor, San Francisco, CA 94107</p>
+          <p><Link to="tel:415-201-6370" className="text-gray-600 hover:text-gray-500">415-201-6370</Link><br />
+            <Link to="mailto:hello@omnifood.com" className="text-gray-600 hover:text-gray-500">hello@omnifood.com</Link>
+          </p>
+        </address>
+      </div>
 
-       <p className="copyright">
-         Copyright &copy; <span className="year">2027</span> by Mtm, Inc.
-         All rights reserved.
-       </p>
-     </div>
+      <div className="md:col-span-1">
+        <p className="text-xl font-semibold mb-8">Account</p>
+        <ul className="flex flex-col gap-8">
+          <li><Link to="/signup" className="text-gray-600 hover:text-gray-500">Create account</Link></li>
+          <li><Link to="/login" className="text-gray-600 hover:text-gray-500">Sign in</Link></li>
+          <li><Link to="/home" className="text-gray-600 hover:text-gray-500">iOS app</Link></li>
+          <li><Link to="/home" className="text-gray-600 hover:text-gray-500">Android app</Link></li>
+        </ul>
+      </div>
 
-     <div className="address-col">
-       <p className="footer-heading">Contact us</p>
-       <address className="contacts">
-         <p className="address">
-           623 Harrison St., 2nd Floor, San Francisco, CA 94107
-         </p>
-         <p>
-           <a className="footer-link" href="tel:415-201-6370">415-201-6370</a><br />
-           <a className="footer-link" href="mailto:hello@omnifood.com">hello@omnifood.com</a>
-         </p>
-       </address>
-     </div>
+      <div className="md:col-span-1">
+        <p className="text-xl font-semibold mb-8">Company</p>
+        <ul className="flex flex-col gap-8">
+          <li><Link to='/About' className="text-gray-600 hover:text-gray-500">About Mtm</Link></li>
+          <li><Link to='/Dashboard' className="text-gray-600 hover:text-gray-500">For Business</Link></li>
+          <li><Link to="/CookingPartners" className="text-gray-600 hover:text-gray-500">Cooking partners</Link></li>
+          <li><Link to="/Carers" className="text-gray-600 hover:text-gray-500">Careers</Link></li>
+        </ul>
+      </div>
 
-     <nav className="nav-col">
-       <p className="footer-heading">Account</p>
-       <ul className="footer-nav">
-         <li><Link to="/signup" class="footer-link" >Create account</Link></li>
-         <li><Link to="/login" class="footer-link" >Sign in</Link></li>
-         <li><Link to="/home" class="footer-link" >iOS app</Link></li>
-         <li><Link to="/home" class="footer-link" >Android app</Link></li>
-       </ul>
-     </nav>
-
-     <nav className="nav-col">
-       <p className="footer-heading">Company</p>
-       <ul className="footer-nav">
-         <li><Link to='/About' class="footer-link">About Mtm</Link></li>
-         <li><Link to='/Dashboard' class="footer-link">For Business</Link></li>
-         <li><Link to="/CookingPartners" class="footer-link">Cooking partners</Link></li>
-         <li><Link to="/Carers" class="footer-link">Careers</Link></li>
-       </ul>
-     </nav>
-
-     <nav className="nav-col">
-       <p className="footer-heading">Resources</p>
-       <ul className="footer-nav">
-         <li><Link to="/Term" class="footer-link">Recipe directory </Link></li>
-         <li><Link to="/Help" class="footer-link" >Help center</Link></li>
-         <li><Link to="/Term" class="footer-link" >Privacy & terms</Link></li>
-       </ul>
-     </nav>
-   </div>
-
- </footer>
+      <div className="md:col-span-1">
+        <p className="text-xl font-semibold mb-8">Resources</p>
+        <ul className="flex flex-col gap-8">
+          <li><Link to="/Term" className="text-gray-600 hover:text-gray-500">Recipe directory</Link></li>
+          <li><Link to="/Help" className="text-gray-600 hover:text-gray-500">Help center</Link></li>
+          <li><Link to="/Term" className="text-gray-600 hover:text-gray-500">Privacy & terms</Link></li>
+        </ul>
+      </div>
+    </div>
+  </footer>
   );
 }
 
--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -1,54 +1,64 @@
 import { AiOutlineShoppingCart } from "react-icons/ai";
 import { Link } from "react-router-dom";
-import logo from "../../assets/Images/logo.jpeg";
-import "./Header.css"
-import "../../styles/Common.css"
-import "../../styles/responsive.css"
+import "./Header.css";
+import "../../styles/Common.css";
+import "../../styles/responsive.css";
+
+import { BsListUl, BsPersonBoundingBox } from "react-icons/bs";
 const Header = () => {
   return (
     <header className="header">
-    <Link to="/home">
-      <img className="logo" src={logo} alt="Omnifood logo" />
-    </Link>
+      <Link to="/home">
+        <h1 className="text-black font-semibold text-3xl">
+          Mtm Metales <span className="text-orange-500">&</span> Gift House
+        </h1>
+      </Link>
 
-    <nav className="main-nav">
-      <ul className="main-nav-list">
-        <li>
-          <Link className="main-nav-link" to="/home">
-            Home
-          </Link>
-        </li>
-        <li>
-          <Link className="main-nav-link" to="/foods">
-            All Foods
-          </Link>
-        </li>
-        <li>
-          <Link className="main-nav-link" to="/Contact">
-            Contact Us
-          </Link>
-        </li>
+      <nav className="main-nav">
+        <ul className="main-nav-list">
+          <li>
+            <Link className="main-nav-link" to="/home">
+              Home
+            </Link>
+          </li>
+          <li>
+            <Link className="main-nav-link" to="/foods">
+              Shop
+            </Link>
+          </li>
+          <li>
+            <Link className="main-nav-link" to="/Contact">
+              Contact Us
+            </Link>
+          </li>
 
-        <li>
-          <Link className="main-nav-link" to="/cart">
-            10
-            <AiOutlineShoppingCart className="cursor-pointer" size={30} />
-          </Link>
-        </li>
+          <li>
+            <Link className="main-nav-link" to="/cart">
+              10
+              <AiOutlineShoppingCart className="cursor-pointer" size={30} />
+            </Link>
+          </li>
 
-        <li>
-          <Link className="main-nav-link nav-cta" to="/login">
-            Join Now
-          </Link>
-        </li>
-      </ul>
-    </nav>
+          <div className="bg-white w-24 cursor-pointer  h-11 rounded-full border-gray-600 border shadow-lg ">
+            <div className="flex justify-between p-2">
+              <BsListUl size={15} />
+              <BsPersonBoundingBox size={15} />
+            </div>
+          </div>
 
-    <button className="btn-mobile-nav">
-      <ion-icon class="icon-mobile-nav" name="menu-outline"></ion-icon>
-      <ion-icon class="icon-mobile-nav" name="close-outline"></ion-icon>
-    </button>
-  </header>
+          <li>
+            <Link className="main-nav-link nav-cta" to="/login">
+              Join Now
+            </Link>
+          </li>
+        </ul>
+      </nav>
+
+      <button className="btn-mobile-nav">
+        <ion-icon class="icon-mobile-nav" name="menu-outline"></ion-icon>
+        <ion-icon class="icon-mobile-nav" name="close-outline"></ion-icon>
+      </button>
+    </header>
   );
 };
 
