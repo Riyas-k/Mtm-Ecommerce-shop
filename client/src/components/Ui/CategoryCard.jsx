2b9a86130ea1ea087b93add11f9c718ae3a98ceb
Athif Binu <binuathif@gmail.com>
2023-09-20 00:03:34 +0530

    category ui bug fix

--- client/src/components/Ui/CategoryCard.jsx
+++ client/src/components/Ui/CategoryCard.jsx
@@ -1,37 +1,130 @@
-// eslint-disable-next-line no-unused-vars
-import React from "react";
-
 import cat from "../../assets/Images/test/categorys/51RWBcopOGL._SX300_SY300_QL70_FMwebp_.webp";
 
 const CategoryCard = () => {
   return (
-       <div>
-              <div className="">
-     <img className="rounded-lg h-56 mb-4" src={cat} alt="" />
-          <h3 className="text-black font-semibold">Mate</h3>
+    <>
+      <div className="flex flex-col">
+        <img className="rounded-lg h-56 mb-2" src={cat} alt="" />
+        <h3 className="text-black font-semibold text-center text-xl">
+          Steel Items
+        </h3>
       </div>
 
-      <div className="items-center">
-        <img className="rounded-lg h-56 mb-4" src={cat} alt="" />
-          <h3 className="text-black font-semibold">Mate</h3>
+      <div className="flex flex-col">
+        <img className="rounded-lg h-56 mb-2" src={cat} alt="" />
+        <h3 className="text-black font-semibold text-center">Mate</h3>
       </div>
 
-      <div className="items-center">
-        <img className="rounded-lg h-56 mb-4" src={cat} alt="" />
-          <h3 className="text-black font-semibold">Mate</h3>
+      <div className="flex flex-col">
+        <img className="rounded-lg h-56 mb-2" src={cat} alt="" />
+        <h3 className="text-black font-semibold text-center">Mate</h3>
       </div>
 
-      <div className="items-center">
-        <img className="rounded-lg h-56 mb-4" src={cat} alt="" />
-          <h3 className="text-black font-semibold">Mate</h3>
+      <div className="flex flex-col">
+        <img className="rounded-lg h-56 mb-2" src={cat} alt="" />
+        <h3 className="text-black font-semibold text-center">Mate</h3>
       </div>
 
-
-      <div className="items-center">
-        <img className="rounded-lg h-56 mb-4" src={cat} alt="" />
-          <h3 className="text-black font-semibold">Mate</h3>
+      <div className="flex flex-col">
+        <img className="rounded-lg h-56 mb-2" src={cat} alt="" />
+        <h3 className="text-black font-semibold text-center">Mate</h3>
       </div>
-       </div>
+
+      <div className="flex flex-col">
+        <img className="rounded-lg h-56 mb-2" src={cat} alt="" />
+        <h3 className="text-black font-semibold text-center">Mate</h3>
+      </div>
+
+      <div className="flex flex-col">
+        <img className="rounded-lg h-56 mb-2" src={cat} alt="" />
+        <h3 className="text-black font-semibold text-center">Mate</h3>
+      </div>
+
+      <div className="flex flex-col">
+        <img className="rounded-lg h-56 mb-2" src={cat} alt="" />
+        <h3 className="text-black font-semibold text-center">Mate</h3>
+      </div>
+
+      <div className="flex flex-col">
+        <img className="rounded-lg h-56 mb-2" src={cat} alt="" />
+        <h3 className="text-black font-semibold text-center">Mate</h3>
+      </div>
+
+      <div className="flex flex-col">
+        <img className="rounded-lg h-56 mb-2" src={cat} alt="" />
+        <h3 className="text-black font-semibold text-center">Mate</h3>
+      </div>
+
+      <div className="flex flex-col">
+        <img className="rounded-lg h-56 mb-2" src={cat} alt="" />
+        <h3 className="text-black font-semibold text-center">Mate</h3>
+      </div>
+
+      <div className="flex flex-col">
+        <img className="rounded-lg h-56 mb-2" src={cat} alt="" />
+        <h3 className="text-black font-semibold text-center">Mate</h3>
+      </div>
+
+      <div className="flex flex-col">
+        <img className="rounded-lg h-56 mb-2" src={cat} alt="" />
+        <h3 className="text-black font-semibold text-center">Mate</h3>
+      </div>
+
+      <div className="flex flex-col">
+        <img className="rounded-lg h-56 mb-2" src={cat} alt="" />
+        <h3 className="text-black font-semibold text-center">Mate</h3>
+      </div>
+
+      <div className="flex flex-col">
+        <img className="rounded-lg h-56 mb-2" src={cat} alt="" />
+        <h3 className="text-black font-semibold text-center">Mate</h3>
+      </div>
+
+      <div className="flex flex-col">
+        <img className="rounded-lg h-56 mb-2" src={cat} alt="" />
+        <h3 className="text-black font-semibold text-center">Mate</h3>
+      </div>
+
+      <div className="flex flex-col">
+        <img className="rounded-lg h-56 mb-2" src={cat} alt="" />
+        <h3 className="text-black font-semibold text-center">Mate</h3>
+      </div>
+
+      <div className="flex flex-col">
+        <img className="rounded-lg h-56 mb-2" src={cat} alt="" />
+        <h3 className="text-black font-semibold text-center">Mate</h3>
+      </div>
+
+      <div className="flex flex-col">
+        <img className="rounded-lg h-56 mb-2" src={cat} alt="" />
+        <h3 className="text-black font-semibold text-center">Mate</h3>
+      </div>
+
+      <div className="flex flex-col">
+        <img className="rounded-lg h-56 mb-2" src={cat} alt="" />
+        <h3 className="text-black font-semibold text-center">Mate</h3>
+      </div>
+
+      <div className="flex flex-col">
+        <img className="rounded-lg h-56 mb-2" src={cat} alt="" />
+        <h3 className="text-black font-semibold text-center">Mate</h3>
+      </div>
+
+      <div className="flex flex-col">
+        <img className="rounded-lg h-56 mb-2" src={cat} alt="" />
+        <h3 className="text-black font-semibold text-center">Mate</h3>
+      </div>
+
+      <div className="flex flex-col">
+        <img className="rounded-lg h-56 mb-2" src={cat} alt="" />
+        <h3 className="text-black font-semibold text-center">Mate</h3>
+      </div>
+
+      <div className="flex flex-col">
+        <img className="rounded-lg h-56 mb-2" src={cat} alt="" />
+        <h3 className="text-black font-semibold text-center">Mate</h3>
+      </div>
+    </>
   );
 };
 
